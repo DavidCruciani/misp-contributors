0a6ab074b468112f7d1d75178637d05aa71159cf
David Cruciani <david.cruciani@circl.lu>
2026-02-19 14:39:04 +0100

    fix: [generate] style of avatar and add datetime at top of page

--- generate-top.py
+++ generate-top.py
@@ -1,3 +1,4 @@
+import datetime
 import redis
 redcon = redis.StrictRedis(host='localhost', port=6379, db=11, decode_responses=True)
 
@@ -15,12 +16,11 @@ repo_to_skip = ['cti-python-stix2', 'SwiftCodes']
 
 all_contributors = redcon.zcard('topversatile')
 
-output += f"# {all_contributors} Contributors \n\n"
+output += f"# {all_contributors} Contributors ({datetime.datetime.now().strftime('%Y-%m-%d')}) \n\n"
 
 for user in redcon.zrevrange('topversatile', 0, -1):
     gravatar = redcon.get("a:{}".format(user))
-    output += "[![{}]({}){{:height=\"36px\" width=\"36px\"}}]({}{})".format(user, gravatar, url_user,
-                                                     user)
+    output += f"[![{user}]({gravatar} \"{{:height='36px' width='36px'}}\")]({url_user}{user})"
 output += "\n"
 
 all_contributors = redcon.zcard('topcommit')
@@ -29,7 +29,7 @@ output += f"## Top {all_contributors}  contributors per commit \n\n"
 
 for user in redcon.zrevrange('topcommit', 0, -1):
     gravatar = redcon.get("a:{}".format(user))
-    output += "[![{}]({}){{:height=\"36px\" width=\"36px\"}}]({}{})".format(user, gravatar, url_user, user)
+    output += f"[![{user}]({gravatar} \"{{:height='36px' width='36px'}}\")]({url_user}{user})"
 
 output += "\n"
 
@@ -37,12 +37,12 @@ for repository in sorted(redcon.smembers('repositories')):
     if repository in repo_to_skip:
         continue
     card = redcon.zcard('r:{}'.format(repository))
-    output += "# {} with {} contributors \n\n".format(repository, card)
-    output += "The repository [{}]({}{}) is part of the MISP project and has the following top contributors \n\n".format(repository, url_github, repository)
+    output += f"# {repository} with {card} contributors \n\n"
+    output += f"The repository [{repository}]({url_github}{repository}) is part of the MISP project and has the following top contributors \n\n"
     output += "| username | total commits |\n"
     output += "|:--------:|:-------------:|\n"
     for top in redcon.zrevrange('r:{}'.format(repository), 0, -1, withscores=True):
-        output += "|[{}]({}{})|{}|\n".format(top[0],url_user,top[0],int(top[1]))
+        output += f"|[{top[0]}]({url_user}{top[0]})|{int(top[1])}|\n"
     output += "\n"
 
 # print (output)
